c216be696f6abccc5c01f5369f41e76197f5104b
Claudia Reyes <clausreyesbravo@gmail.com>
2023-09-27 00:14:40 -0600

    algoritmo con condicional

---
+++ main.js
@@ -0,0 +1,11 @@
+
+//Algoritmo con condicional
+
+let combo = parseInt(prompt("Cual es tu presupuesto aproximado?"));
+if (combo <= 3000) {alert("Podria interesarte nuestro paquete A");
+    
+}else if ( combo <4000){alert("Podria interesarte nuestro paquetes de servicio B ");
+
+} else { alert("Podri interesarte nuestro paquete premium")}
+
+
