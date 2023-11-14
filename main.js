e41314b63aae758f5c0e18555c0ac28ebd07f1f7
Claudia Reyes <clausreyesbravo@gmail.com>
2023-11-14 10:32:01 -0600

    fetch no resuelto

--- main.js
+++ main.js
@@ -5,43 +5,53 @@ const modalContainer = document.getElementById("modal-container");
 const cantidadCarrito = document.getElementById("cantidadCarrito");
 let carrito = JSON.parse(localStorage.getItem("carrito")) || [];
 
-productos.forEach((product)=> {
-let content = document.createElement("div"); 
-content.className = "card";  
-content.innerHTML = `
-<img src="${product.img}">
-<h3>${product.nombre}</h3
-<p class="price">${product.precio}$</p>
-`;
-shopContent.append(content)
+const getProducts = async() =>{
+    const response = await fetch("data.json");
+    const products =  await response.json();
 
-let comprar = document.createElement("button")
-comprar.innerText = "Comprar";
-comprar.className = "comprar"
-content.append(comprar);
-
-
-comprar.addEventListener("click", () => {
-const repeat = carrito.some((repeatProduct) => repeatProduct.id === product.id);
-if(repeat){
-    carrito.map((prod) => {
-        if(prod.id === prod.id){
-            prod.cantidad++;
+    data.forEach((product)=> {
+        let content = document.createElement("div"); 
+        content.className = "card";  
+        content.innerHTML = `
+        <img src="${product.img}">
+        <h3>${product.nombre}</h3
+        <p class="price">${product.precio}$</p>
+        `;
+        shopContent.append(content);
+        
+        let comprar = document.createElement("button")
+        comprar.innerText = "Comprar";
+        comprar.className = "comprar"
+        content.append(comprar);
+        
+        
+        comprar.addEventListener("click", () => {
+        const repeat = carrito.some((repeatProduct) => repeatProduct.id === product.id);
+        if(repeat){
+            carrito.map((prod) => {
+                if(prod.id === prod.id){
+                    prod.cantidad++;
+                }
+            });
+        }else{
+            carrito.push({
+                id: product.id,
+                img: product.img,
+                nombre: product.nombre,
+                precio: product.precio,
+                cantidad: product.cantidad,
+            }); 
+        carritoCounter();
+        saveLocal();
         }
-    });
-}else{
-    carrito.push({
-        id: product.id,
-        img: product.img,
-        nombre: product.nombre,
-        precio: product.precio,
-        cantidad: product.cantidad,
-    }); 
-carritoCounter();
-saveLocal();
-}
-});
-});
+        });
+        });
+            
+    };
+
+getProducts();
+
+
 
 const saveLocal = ()=> {
 localStorage.setItem("carrito", JSON.stringify(carrito));
